b33eda023bce53b380f229d1e96eaf22171c3411
Shreyansh <honasa@HCPL-MAC-005.local>
2021-02-17 14:48:03 +0530

    Updates

---
+++ precache-manifest.e19cf089c1441544a935cba74c08df40.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3956d95ef4076d23e88e7e588f385946",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a5d6f2469985242ae2a9",
+    "url": "/my-app/static/css/2.45f551db.chunk.css"
+  },
+  {
+    "revision": "ef424d70534522faf87f",
+    "url": "/my-app/static/css/main.14ec5843.chunk.css"
+  },
+  {
+    "revision": "a5d6f2469985242ae2a9",
+    "url": "/my-app/static/js/2.78ce5578.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-app/static/js/2.78ce5578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef424d70534522faf87f",
+    "url": "/my-app/static/js/main.2b6d8c55.chunk.js"
+  },
+  {
+    "revision": "2de92843ebe1024ae347",
+    "url": "/my-app/static/js/runtime-main.070e71f6.js"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/my-app/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/my-app/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/my-app/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/my-app/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  }
+]);
